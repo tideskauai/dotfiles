607e699c8daf18f8eb84527b3a37626d675db947
ballmerpeak <xkcd.ballmer@gmail.com>
2012-05-18 13:39:52 -0400

    dotfiles: renamed some files

--- .config/owncfg/xmobar/netstats.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-received=$(cat /sys/class/net/eth0/statistics/rx_bytes)
-transferred=$(cat /sys/class/net/eth0/statistics/tx_bytes)
-
-echo -n "<fc=#5d7caf>$(( $received/1024/1024 )) </fc>MB "
-echo "<fc=#FBA4A8>$(( $transferred/1024/1024 )) </fc>MB"
